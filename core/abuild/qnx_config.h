27e433809c784aa2f12ca2163b5c63851897bf38
Leo Shen <i@szclsya.me>
2025-12-03 15:15:19 -0500

    initial release

---
+++ core/abuild/qnx_config.h
@@ -0,0 +1,9 @@
+#ifndef ABUILD_QNX_CONFIG_H
+#define ABUILD_QNX_CONFIG_H
+
+// Extra cofigurations made for self-hosted abuild on QNX
+
+#define QNX_SYS_ROOT "/system"
+#define QNX_USR_ROOT "$HOME"
+
+#endif // ABUILD_QNX_CONFIG_H
